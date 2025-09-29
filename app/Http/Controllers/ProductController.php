c1596b0ae7dc44166d0b1cc7b20d6818f8496e26
Hamza Firdaus <hamza.firdaus@ihc.id>
2025-09-29 19:53:49 +0700

    init files from R4ynarr/shoppe

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use PhpParser\Node\Stmt\TryCatch;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $token = env('TOKEN_PASS');
+
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'token' => 'required|string|max:50',
+            ],
+            [
+                'token.required' => "Token wajib diisi"
+            ]
+        );
+
+        if ($validator->fails()) {
+            $errors = $validator->errors()->all();
+            $errorMessages = implode(', ', $errors);
+            return $this->responseJson($errorMessages, 400);
+        }
+
+        if ($request->token != $token) {
+            return $this->responseJson("Token invalid", 400);
+        }
+
+        $products = Product::all();
+        return $this->responseJson("Ok", 200, $products);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'name' => 'required|string|max:50',
+                'price' => 'required|numeric',
+                'stock' => 'required|integer|min:0',
+            ],
+            [
+                'name.required' => 'name must be filled',
+                'name.string' => 'name must be filled by array ',
+                'name.max' => 'max 50 chars',
+                'price.required' => 'price must be filled',
+                'price.required' => 'price must be a number',
+                'stock.required' => 'stock must be filled',
+                'stock.integer' => 'stock must be filled by integer',
+                'stock.min' => 'stock must be positive',
+            ]
+        );
+
+        if ($validator->fails()) {
+            $errors = $validator->errors()->all();
+            $errorMessages = implode(', ', $errors);
+            return response()->json(['message' => $errorMessages], 201);
+        }
+
+        $validated = $validator->validated();
+
+        product::create($request->all());
+        return response()->json(['message' => 'produk berhasil disimpan'], 201);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Request $request, string $id)
+    {
+
+        $product = Product::findOrFail($id);
+
+        if ($product) {
+            return $this->responseJson("Ok", 200, $product);
+        } else {
+            return response()->json(['message' => 'produk tidak ditemukan'], 404);
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+
+        $validator = Validator::make(
+            $request->all(),
+            [
+
+                'name' => 'required|string|max:50',
+                'price' => 'required|numeric',
+                'stock' => 'required|integer|min:0',
+
+            ],
+            [
+                'name.required' => 'name must be filled',
+                'name.string' => 'name must be filled by array ',
+                'name.max' => 'max 50 chars',
+                'price.required' => 'price must be filled',
+                'price.required' => 'price must be a number',
+                'stock.required' => 'stock must be filled',
+                'stock.integer' => 'stock must be filled by integer',
+                'stock.min' => 'stock must be positive',
+            ]
+        );
+
+        if ($validator->fails()) {
+            $errors = $validator->errors()->all();
+            $errorMessages = implode(', ', $errors);
+            return $this->responseJson($errorMessages, 400);
+        }
+
+        $validated = $validator->validated();
+
+        product::create($request->all());
+        return response()->json(['message' => 'produk berhasil disimpan'], 201);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+
+    {
+        try {
+            //code...
+            $product = Product::findOrFail($id);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return $this->responseJson('produk tidak ditemukan', 404);
+        }
+
+
+
+        $product->delete();
+        return $this->responseJson('produk berhasil dihapus', 200, null);
+    }
+
+    public function responseJson($message, $status = 200, $data = null)
+    {
+        return response()->json([
+            'data' => $data,
+            'message' => $message
+        ], $status);
+    }
+}
---
+++ database/seeders/ProductsTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ProductsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $now = now();
+
+        DB::table('products')->insert([
+            'name' => 'Produk A',
+            'price' => 100,
+            'stock' => 50,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+        DB::table('products')->insert([
+            'name' => 'Produk B',
+            'price' => 150,
+            'stock' => 30,
+            'created_at' => $now,
+            'updated_at' => $now,
+        ]);
+    }
+}
